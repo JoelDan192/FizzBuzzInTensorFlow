1827468c72df9720ca284c42082436bf325c48ae
Joel Castellon <joel.castellonarevalo@epfl.ch>
2016-10-20 22:04:44 +0200

    SGD fix on batch size and convergence

--- labs/ex02/template/stochastic_gradient_descent.py
+++ labs/ex02/template/stochastic_gradient_descent.py
@@ -16,10 +16,11 @@ def compute_loss(y, tx, w):
     return (1/(2*n))*np.dot(e.T,e)
 
 
-def compute_stoch_gradient(y, tx, w):
+def compute_stoch_gradient(y, tx, w, batch_size):
     """Compute a stochastic gradient for batch data."""
-    yn, xn = next(batch_iter(y,tx,1))
-    return -(yn-np.dot(xn,w))*xn.T
+    yn, xn = next(batch_iter(y,tx,batch_size))
+    residuals_batch = np.array(list(map(lambda tup: -(tup[0]-np.dot(tup[1],w.T)), zip(yn,xn))))
+    return (1/batch_size)*np.dot(residuals_batch, xn)
 
 
 def stochastic_gradient_descent(
@@ -29,11 +30,10 @@ def stochastic_gradient_descent(
     ws = [initial_w]
     losses = []
     w = initial_w
-    for n_iter in range(max_iters):
-        g = None
-        for _ in batch_size:
-            g+= compute_stoch_gradient(y,tx,w)
-        g = 1/(batch_size)*g
+    
+    for n_iter in range(max_epochs):
+        g = np.array([0,0])
+        g = compute_stoch_gradient(y,tx,w,batch_size)
         loss = compute_loss(y, tx, w)
         w = w - gamma*g
         # store w and loss
@@ -41,4 +41,5 @@ def stochastic_gradient_descent(
         losses.append(loss)
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
     return losses, ws
