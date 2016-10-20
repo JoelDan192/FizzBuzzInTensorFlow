a5f9e22a38a2b5c07a5f5c40db2d7be627143e91
Joel Castellon <joel.castellonarevalo@epfl.ch>
2016-10-20 17:03:45 +0200

    gradient descent implemented

--- labs/ex02/template/gradient_descent.py
+++ labs/ex02/template/gradient_descent.py
@@ -7,34 +7,34 @@ import numpy as np
 import costs
 
 
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y-np.dot(tx,w)
+    n = e.shape[0]
+    return (1/(2*n))*np.dot(e.T,e)
+
+
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute gradient and loss
-    # ***************************************************
-    raise NotImplementedError
+    e = y - np.dot(tx,w)
+    n = e.shape[0]
+    return -(1/n)*np.dot(tx.T,e)
 
-
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
+def gradient_descent(y, tx, initial_w, max_iters, gamma): 
     """Gradient descent algorithm."""
     # Define parameters to store w and loss
     ws = [initial_w]
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: compute gradient and loss
-        # ***************************************************
-        raise NotImplementedError
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: update w by gradient
-        # ***************************************************
-        raise NotImplementedError
+        g = compute_gradient(y,tx,w)
+        loss = compute_loss(y, tx, w)
+        w = w - gamma*g
         # store w and loss
-        ws.append(w)
+        ws.append(np.copy(w))
         losses.append(loss)
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
--- labs/ex02/template/stochastic_gradient_descent.py
+++ labs/ex02/template/stochastic_gradient_descent.py
@@ -6,19 +6,39 @@ Stochastic Gradient Descent
 from helpers import batch_iter
 
 
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y-np.dot(tx,w)
+    n = e.shape[0]
+    return (1/(2*n))*np.dot(e.T,e)
+
+
 def compute_stoch_gradient(y, tx, w):
     """Compute a stochastic gradient for batch data."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient computation.
-    # ***************************************************
+    yn, xn = next(batch_iter(y,tx,1))
+    return -(yn-np.dot(xn,w))*xn.T
 
 
 def stochastic_gradient_descent(
         y, tx, initial_w, batch_size, max_epochs, gamma):
     """Stochastic gradient descent algorithm."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient descent.
-    # ***************************************************
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        g = None
+        for _ in batch_size:
+            g+= compute_stoch_gradient(y,tx,w)
+        g = 1/(batch_size)*g
+        loss = compute_loss(y, tx, w)
+        w = w - gamma*g
+        # store w and loss
+        ws.append(np.copy(w))
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     return losses, ws
