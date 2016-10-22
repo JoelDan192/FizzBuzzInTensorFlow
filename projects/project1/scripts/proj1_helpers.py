f24831c518df62fd455850613d5a4b0616aa3626
Joel Castellon <joel.castellonarevalo@epfl.ch>
2016-10-22 12:33:09 +0200

    First submission tested

--- projects/project1/scripts/proj1_helpers.py
+++ projects/project1/scripts/proj1_helpers.py
@@ -2,7 +2,7 @@
 """some helper functions for project 1."""
 import csv
 import numpy as np
-
+from logistic_regression import sigmoid
 
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
@@ -32,6 +32,11 @@ def predict_labels(weights, data):
     
     return y_pred
 
+def predict_labels_logistic(weights, data):
+    y_pred = sigmoid(np.dot(data,weights))
+    y_pred[np.where(y_pred<0.5)] = -1
+    y_pred[np.where(y_pred>=0.5)] = 1
+    return y_pred
 
 def create_csv_submission(ids, y_pred, name):
     """
